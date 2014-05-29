1dddd4f03843774ba78365a1f42a30498846eb1b
Alun Evans <alun.evans@xockets.com>
2014-05-29 10:11:58 -0700

    text mode for scratch

--- init.el
+++ init.el
@@ -62,16 +62,17 @@
 						 *init-start-time*))))
 
 (custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
+  ;; custom-set-variables was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
  '(inhibit-startup-screen t)
+ '(initial-major-mode (quote text-mode))
  '(user-full-name "Alun Evans")
  '(user-mail-address "alun.evans@xockets.com"))
 (custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
+  ;; custom-set-faces was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
  )
