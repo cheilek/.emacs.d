d5941406b95b19abec6e886503f94a7c56e3cba3
Alun Evans <alun.evans@xockets.com>
2014-11-05 10:09:20 -0800

    Switched to github f markdown.

--- markdown.el
+++ markdown.el
@@ -6,4 +6,5 @@
 (autoload 'markdown-mode "markdown-mode"
   "Major mode for editing Markdown files" t)
 
-(add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.md\\'" . gfm-mode))
+;;(add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
