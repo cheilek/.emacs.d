8b969f5957d71d42715ce09dd9a6134bae797ff6
Alun Evans <alun.evans@xockets.com>
2014-11-05 10:37:46 -0800

    Make gfm actually load...

--- markdown.el
+++ markdown.el
@@ -3,8 +3,12 @@
        (list "~/.emacs.d/markdown-mode/")
        load-path))
 
-(autoload 'markdown-mode "markdown-mode"
+;; (autoload 'markdown-mode "markdown-mode"
+;;   "Major mode for editing Markdown files" t)
+
+;; (add-to-list 'auto-mode-alist '("\\.md$" . markdown-mode))
+
+(autoload 'gfm-mode "markdown-mode"
   "Major mode for editing Markdown files" t)
 
-(add-to-list 'auto-mode-alist '("\\.md\\'" . gfm-mode))
-;;(add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.md$" . gfm-mode))
