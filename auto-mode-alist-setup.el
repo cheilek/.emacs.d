7a4d90efd71296ce31f0799762ba3fe407e32f8e
Alun Evans <alun.evans@xockets.com>
2015-02-02 17:02:09 -0800

    Added .mk

--- auto-mode-alist-setup.el
+++ auto-mode-alist-setup.el
@@ -3,6 +3,7 @@
 (add-to-list 'auto-mode-alist '("_defconfig$" . conf-mode))
 (add-to-list 'auto-mode-alist '("/Kbuild$" . makefile-mode))
 (add-to-list 'auto-mode-alist '("\\.make$" . makefile-mode))
+(add-to-list 'auto-mode-alist '("\\.mk$" . makefile-mode))
 
 ;; (add-to-list 'auto-mode-alist
 ;;              '("/\\(rfc\\|std\\)[0-9]+\\.txt\\'" . rfcview-mode))
