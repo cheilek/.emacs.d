81185793a01979e9b2fb8b9e60c41352e6cc8c92
Alun Evans <alun.evans@xockets.com>
2015-12-03 18:57:31 -0800

    I like 4 spaces and no tabs, even for xml.

--- sgml-setup.el
+++ sgml-setup.el
@@ -6,6 +6,13 @@
 ;;(require 'emmet-mode)
 ;;(add-hook 'sgml-mode-hook 'emmet-mode) ;; Auto-start on any markup modes
 
+(add-hook 'nxml-mode-hook
+          (lambda ()
+	    (setq nxml-child-indent 4)
+            (setq indent-tabs-mode nil)
+	    )
+	  )
+
 (add-hook 'sgml-mode-hook
           (lambda ()
 	    (setq sgml-basic-offset 4)
